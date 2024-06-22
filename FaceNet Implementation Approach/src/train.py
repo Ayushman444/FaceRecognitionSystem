4dcbe1fec6c0fc5182d4f652a9b315bb81b952d5
IWantToBeVS <sharma02vaibhav@gmail.com>
2024-06-22 22:38:27 +0530

    Added Data Generator

--- FaceNet Implementation Approach/src/train.py
+++ FaceNet Implementation Approach/src/train.py
@@ -4,7 +4,7 @@ import keras
 import pickle
 import random
 from Models.FaceNet_second_model_proposition import *
-
+from data_generator import *
 
 ''' THIS CODE SEGMENT IS TO DOWNLOAD THE LFW DATA ALREADY PROCESSED BY LFW2PKL.py'''
 # from google.colab import drive
@@ -16,67 +16,67 @@ from Models.FaceNet_second_model_proposition import *
 #       drive.flush_and_unmount()
 # driver = Drive()
 
-class load_and_refine_data:
+# class load_and_refine_data:
 
-  def load_data(self):
+#   def load_data(self):
 
-    with open('data_test.pkl', 'rb') as f:
-      test_df = pickle.load(f)
+#     with open('data_test.pkl', 'rb') as f:
+#       test_df = pickle.load(f)
 
-    with open('data_train.pkl', 'rb') as f:
-      train_df = pickle.load(f)
+#     with open('data_train.pkl', 'rb') as f:
+#       train_df = pickle.load(f)
 
-    x_train = train_df['image']
-    y_train = train_df['person']
+#     x_train = train_df['image']
+#     y_train = train_df['person']
 
-    x_test = test_df['image']
-    y_test = test_df['person']
+#     x_test = test_df['image']
+#     y_test = test_df['person']
 
-    # Loop through each image in the Series
-    for index, image in x_train.items():  # Access elements using items()
-      x_train.loc[index] = self.remove_extra_dim(image)  # Modify Series in-place
+#     # Loop through each image in the Series
+#     for index, image in x_train.items():  # Access elements using items()
+#       x_train.loc[index] = self.remove_extra_dim(image)  # Modify Series in-place
 
-    for index, image in x_test.items():  # Access elements using items()
-      x_test.loc[index] = self.remove_extra_dim(image)  # Modify Series in-place
+#     for index, image in x_test.items():  # Access elements using items()
+#       x_test.loc[index] = self.remove_extra_dim(image)  # Modify Series in-place
 
-    classes = set()
-    for i in y_train:
-      classes.add(i)
+#     classes = set()
+#     for i in y_train:
+#       classes.add(i)
 
-    classes = list(classes)
+#     classes = list(classes)
 
-    return x_train, y_train, x_test, y_test, classes
+#     return x_train, y_train, x_test, y_test, classes
 
-  def remove_extra_dim(self, image):
-    try:
-        return np.squeeze(image, axis=0)  # Efficient removal if size 1
-    except ValueError:
-        # Extra dimension not found, return the original data
-        print('failed')
-        return image
+#   def remove_extra_dim(self, image):
+#     try:
+#         return np.squeeze(image, axis=0)  # Efficient removal if size 1
+#     except ValueError:
+#         # Extra dimension not found, return the original data
+#         print('failed')
+#         return image
 
 
-  def triplet_generator(self, batch_size=64):
-    x_train, y_train, x_test, y_test, classes = self.load_data()
-    while True:
-      a = []
-      p = []
-      n = []
-      for _ in range(batch_size):
-        found = True
-        while(found):
-          pos_neg = random.sample(classes, 2)
-          try:
-            positive_samples = random.sample(list (x_train [y_train==pos_neg [0]]), 2)
-            negative_sample= random.choice(list (x_train [y_train == pos_neg [1]]))
-            found = False
-          except:
-            continue
-        a.append(positive_samples [0])
-        p.append(positive_samples [1])
-        n.append(negative_sample)
+#   def triplet_generator(self, batch_size=64):
+#     x_train, y_train, x_test, y_test, classes = self.load_data()
+#     while True:
+#       a = []
+#       p = []
+#       n = []
+#       for _ in range(batch_size):
+#         found = True
+#         while(found):
+#           pos_neg = random.sample(classes, 2)
+#           try:
+#             positive_samples = random.sample(list (x_train [y_train==pos_neg [0]]), 2)
+#             negative_sample= random.choice(list (x_train [y_train == pos_neg [1]]))
+#             found = False
+#           except:
+#             continue
+#         a.append(positive_samples [0])
+#         p.append(positive_samples [1])
+#         n.append(negative_sample)
 
-      yield ([np.array(a), np.array(p), np.array(n)], np.zeros((batch_size, 1)).astype("float32"))
+#       yield ([np.array(a), np.array(p), np.array(n)], np.zeros((batch_size, 1)).astype("float32"))
 
 face_net = FaceNet_Model()
 
